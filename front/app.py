3f70962fcdec7281910bb3aea3b36bfa195072ab
Audric-S <sabatieraudric@gmail.com>
2024-06-07 09:05:51 +0200

    :sparkles: store dataframe in Main

--- front/app.py
+++ front/app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from pages.load_data import load_data_page
+from pages.analyze_data_page import analyze_data_page 
 
 def main():
     st.set_page_config(page_title="Streamlit App", page_icon="📊", layout="centered")
     
     st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Load Data"])
-
+    page = st.sidebar.radio("Go to", ["Load Data", "Analyze Data"])
+    
     if page == "Load Data":
         load_data_page()
+    elif page == "Analyze Data":
+        analyze_data_page()
 
 if __name__ == "__main__":
     main()
--- front/pages/load_data.py
+++ front/pages/load_data.py
@@ -8,26 +8,32 @@ def load_data_page():
     
     if uploaded_file is not None:
         try:
-            na_values = ["", " ", "NA", "N/A", "na", "n/a", "null", "NULL", "-", "--"]
-            data = pd.read_csv(uploaded_file, na_values=na_values)
+            data = pd.read_csv(uploaded_file)
+            st.session_state['data'] = data
+            st.success('File successfully uploaded.')
             
-            st.subheader("Premières 3 lignes:")
-            st.dataframe(data.head(3))
-            
-            st.subheader("Dernières 3 lignes:")
-            st.dataframe(data.tail(3))
-            
-            num_rows, num_cols = data.shape
-            st.subheader("Informations sur le fichier CSV")
-            st.write(f"Nombre de lignes: {num_rows}")
-            st.write(f"Nombre de colonnes: {num_cols}")
-            
-            st.write("Noms des colonnes:")
-            st.write(list(data.columns))
-            
-            missing_values = data.isnull().sum()
-            st.subheader("Valeurs manquantes par colonne:")
-            st.write(missing_values)
+            if data.empty:
+                st.warning("Le fichier CSV est vide.")
+            else:
+                na_values = ["", " ", "NA", "N/A", "na", "n/a", "null", "NULL", "-", "--"]
+                
+                st.subheader("Premières 3 lignes:")
+                st.dataframe(data.head(3))
+                
+                st.subheader("Dernières 3 lignes:")
+                st.dataframe(data.tail(3))
+                
+                num_rows, num_cols = data.shape
+                st.subheader("Informations sur le fichier CSV")
+                st.write(f"Nombre de lignes: {num_rows}")
+                st.write(f"Nombre de colonnes: {num_cols}")
+                
+                st.write("Noms des colonnes:")
+                st.write(list(data.columns))
+                
+                missing_values = data.isnull().sum()
+                st.subheader("Valeurs manquantes par colonne:")
+                st.write(missing_values)
         except Exception as e:
             st.error(f"Erreur lors de la lecture du fichier CSV: {e}")
     else:
