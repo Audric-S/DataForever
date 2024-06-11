e3782fd09e7b4c7d6c6579e99185953f42ba8613
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-06-11 16:04:52 +0200

    :hammer: add: visualisation of data

--- front/pages/data_visualisation.py
+++ front/pages/data_visualisation.py
@@ -1,22 +1,57 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+
+############################### Constante #####################################
+
+visulasiation_options = ['HISTOGRAM', 'COURBE', 'MODE']
+
+############################### Functions #####################################
+
+def get_columns_for_mode(df, mode):
+    if mode in ['HISTOGRAM', 'COURBE']:
+        return df.select_dtypes(include='number').columns
+    elif mode == 'MODE':
+        return df.select_dtypes(include='object').columns
+    else:
+        return None
+
+def courbe(df, columns):
+    column = st.selectbox('Sélectionner la colonne à afficher', columns)
+    st.line_chart(df[column])
+
+def histogram(df, columns):
+    column = st.selectbox('Sélectionner la colonne à afficher', columns)
+    st.bar_chart(df[column])
+
+def mode(df, columns):
+    column = st.selectbox('Sélectionner la colonne à afficher', columns)
+    st.write(df[column].mode())
+
+def displayed_figure(df, selectedMode):
+    if selectedMode:
+        if selectedMode == 'HISTOGRAM':
+            columns = get_columns_for_mode(df, selectedMode)
+            histogram(df, columns)
+        elif selectedMode == 'COURBE':
+            columns = get_columns_for_mode(df, selectedMode)
+            courbe(df, columns)
+        elif selectedMode == 'MODE':
+            columns = get_columns_for_mode(df, selectedMode)
+            mode(df, columns)
+        else:
+            st.write('Mode de visualisation non pris en charge')
+
+
+############################## Using part #####################################
 
 st.title('Data Visualisation')
 
-df = pd.DataFrame({
-    'x': [1, 2, 3, 4, 5],
-    'y': [0, 10, 20, 10, 0]
-})
+# Lire le fichier CSV
+df = pd.read_csv('1000_cryptos.csv')
 
-# Afficher un histogramme pour chaque colonne
-for column in df:
-    st.subheader(f'Histogramme pour {column}')
-    st.bar_chart(df[column])
+# Supprimer la première colonne
+df = df.drop(columns=df.columns[0])
 
-# Afficher une boîte à moustaches pour chaque colonne
-for column in df:
-    st.subheader(f'Boîte à moustaches pour {column}')
-    fig, ax = plt.subplots()
-    ax.boxplot(df[column])
-    st.pyplot(fig)
+# Choose the mode of visualisation
+visualisation_mode = st.selectbox('Sélectionner le type de visualisation', visulasiation_options)
+displayed_figure(df, visualisation_mode)
