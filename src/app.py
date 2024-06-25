8818995563e2891abe40b62ef198152c20ae1e6a
Audric-S <sabatieraudric@gmail.com>
2024-06-25 17:26:38 +0200

    :lipstick: add: change color thems

--- src/app.py
+++ src/app.py
@@ -3,17 +3,15 @@ from pages.load_data import load_data_page
 from pages.data_visualisation import data_visualization
 from pages.data_cleaning import data_cleaning
 from pages.clustering_prediction import main_prediction_clustering
+from streamlit_option_menu import option_menu
 
 def main():
     st.set_page_config(page_title="Data Forever", page_icon="📊", layout="centered")
     
-    st.sidebar.title("Navigation")
+    with st.sidebar:
+        page = option_menu("Menu", ["Load Datas", "Clean Datas", "Visualize Datas", "Prediction"], 
+            icons=['cloud-upload', 'gear', ''], default_index=0)
     
-    nav_container = st.sidebar.container()
-    nav_container.empty() 
-    
-    with nav_container:
-        page = st.radio("Go to", ["Load Datas", "Clean Datas", "Visualize Datas", "Prediction"])
     
     if page == "Load Datas":
         load_data_page()
